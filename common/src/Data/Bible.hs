2c2517f9f06a5b1a518f70d018a537c83ddb2789
3noch <eacameron@gmail.com>
2019-10-06 01:35:37 -0400

    Add Enum and 'to-string' for Bible books

--- common/src/Data/Bible.hs
+++ common/src/Data/Bible.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE FlexibleInstances #-}
-
-{-# OPTIONS_GHC -Wno-orphans #-}
 module Data.Bible where
 
 import Data.Finite (Finite, natToFinite)
@@ -42,7 +40,7 @@ fourToFinite Four_2 = natToFinite (Proxy :: Proxy 1)
 fourToFinite Four_3 = natToFinite (Proxy :: Proxy 2)
 fourToFinite Four_4 = natToFinite (Proxy :: Proxy 3)
 
--- Used as an index for 'BookIndex' to mean "ignore book indexes".
+-- Used as an index for 'BookIndex' to mean"ignore book indexes".
 newtype Ignore a = Ignore ()
 
 type family BookIndex f a where
@@ -98,13 +96,134 @@ deriving instance Read (OldTestament' Ignore)
 deriving instance Read (OldTestament' Identity)
 instance Universe (OldTestament' Ignore) where universe = universeGeneric
 instance Universe.Finite (OldTestament' Ignore)
-instance Universe (OldTestament' Identity) where universe = universeGeneric
+instance Universe (OldTestament' Identity) -- Uses 'Enum' instance
 instance Universe.Finite (OldTestament' Identity)
 instance Bounded (OldTestament' f) where
   minBound = Genesis
   maxBound = Malachi
+instance Enum (OldTestament' Identity) where
+  fromEnum x = case x of
+    Genesis -> 0
+    Exodus -> 1
+    Leviticus -> 2
+    Numbers -> 3
+    Deuteronomy -> 4
+    Joshua -> 5
+    Judges -> 6
+    Ruth -> 7
+    Samuel Two_1 -> 8
+    Samuel Two_2 -> 9
+    Kings Two_1 -> 10
+    Kings Two_2 -> 11
+    Chronicles Two_1 -> 12
+    Chronicles Two_2 -> 13
+    Ezra -> 14
+    Nehemiah -> 15
+    Esther -> 16
+    Job -> 17
+    Psalms -> 18
+    Proverbs -> 19
+    Ecclesiastes -> 20
+    SongOfSolomon -> 21
+    Isaiah -> 22
+    Jeremiah -> 23
+    Lamentations -> 24
+    Ezekiel -> 25
+    Daniel -> 26
+    Hosea -> 27
+    Joel -> 28
+    Amos -> 29
+    Obadiah -> 30
+    Jonah -> 31
+    Micah -> 32
+    Nahum -> 33
+    Habakkuk -> 34
+    Zephaniah -> 35
+    Haggai -> 36
+    Zechariah -> 37
+    Malachi -> 38
+  toEnum x = case x of
+    0 -> Genesis
+    1 -> Exodus
+    2 -> Leviticus
+    3 -> Numbers
+    4 -> Deuteronomy
+    5 -> Joshua
+    6 -> Judges
+    7 -> Ruth
+    8 -> Samuel Two_1
+    9 -> Samuel Two_2
+    10 -> Kings Two_1
+    11 -> Kings Two_2
+    12 -> Chronicles Two_1
+    13 -> Chronicles Two_2
+    14 -> Ezra
+    15 -> Nehemiah
+    16 -> Esther
+    17 -> Job
+    18 -> Psalms
+    19 -> Proverbs
+    20 -> Ecclesiastes
+    21 -> SongOfSolomon
+    22 -> Isaiah
+    23 -> Jeremiah
+    24 -> Lamentations
+    25 -> Ezekiel
+    26 -> Daniel
+    27 -> Hosea
+    28 -> Joel
+    29 -> Amos
+    30 -> Obadiah
+    31 -> Jonah
+    32 -> Micah
+    33 -> Nahum
+    34 -> Habakkuk
+    35 -> Zephaniah
+    36 -> Haggai
+    37 -> Zechariah
+    38 -> Malachi
+    n -> error $"OldTestament has no book at index" <> show n
 
 type OldTestament = OldTestament' Identity
+
+oldTestamentToString :: (Two -> String -> String) -> OldTestament' Identity -> String
+oldTestamentToString showTwo x = case x of
+  Genesis -> "Genesis"
+  Exodus -> "Exodus"
+  Leviticus -> "Leviticus"
+  Numbers -> "Numbers"
+  Deuteronomy -> "Deuteronomy"
+  Joshua -> "Joshua"
+  Judges -> "Judges"
+  Ruth -> "Ruth"
+  Samuel x -> showTwo x "Samuel"
+  Kings x -> showTwo x "Kings"
+  Chronicles x -> showTwo x "Chronicles"
+  Ezra -> "Ezra"
+  Nehemiah -> "Nehemiah"
+  Esther -> "Esther"
+  Job -> "Job"
+  Psalms -> "Psalms"
+  Proverbs -> "Proverbs"
+  Ecclesiastes -> "Ecclesiastes"
+  SongOfSolomon -> "Song of Solomon"
+  Isaiah -> "Isaiah"
+  Jeremiah -> "Jeremiah"
+  Lamentations -> "Lamentations"
+  Ezekiel -> "Ezekiel"
+  Daniel -> "Daniel"
+  Hosea -> "Hosea"
+  Joel -> "Joel"
+  Amos -> "Amos"
+  Obadiah -> "Obadiah"
+  Jonah -> "Jonah"
+  Micah -> "Micah"
+  Nahum -> "Nahum"
+  Habakkuk -> "Habakkuk"
+  Zephaniah -> "Zephaniah"
+  Haggai -> "Haggai"
+  Zechariah -> "Zechariah"
+  Malachi -> "Malachi"
 
 data NewTestament' f
   = Matthew
@@ -139,13 +258,99 @@ deriving instance Read (NewTestament' Ignore)
 deriving instance Read (NewTestament' Identity)
 instance Universe (NewTestament' Ignore) where universe = universeGeneric
 instance Universe.Finite (NewTestament' Ignore)
-instance Universe (NewTestament' Identity) where universe = universeGeneric
+instance Universe (NewTestament' Identity) -- Uses 'Enum' instance
 instance Universe.Finite (NewTestament' Identity)
 instance Bounded (NewTestament' f) where
   minBound = Matthew
   maxBound = Revelation
+instance Enum (NewTestament' Identity) where
+  fromEnum x = case x of
+    Matthew -> 0
+    Mark -> 1
+    Luke -> 2
+    John -> 3
+    Acts -> 4
+    Romans -> 5
+    Corinthians Two_1 -> 6
+    Corinthians Two_2 -> 7
+    Galatians -> 8
+    Ephesians -> 9
+    Philippians -> 10
+    Colossians -> 11
+    Thessalonians Two_1 -> 12
+    Thessalonians Two_2 -> 13
+    Timothy Two_1 -> 14
+    Timothy Two_2 -> 15
+    Titus -> 16
+    Philemon -> 17
+    Hebrews -> 18
+    James -> 19
+    Peter Two_1 -> 20
+    Peter Two_2 -> 21
+    JohnEpistle Three_1 -> 22
+    JohnEpistle Three_2 -> 23
+    JohnEpistle Three_3 -> 24
+    Jude -> 25
+    Revelation -> 26
+  toEnum x = case x of
+    0 -> Matthew
+    1 -> Mark
+    2 -> Luke
+    3 -> John
+    4 -> Acts
+    5 -> Romans
+    6 -> Corinthians Two_1
+    7 -> Corinthians Two_2
+    8 -> Galatians
+    9 -> Ephesians
+    10 -> Philippians
+    11 -> Colossians
+    12 -> Thessalonians Two_1
+    13 -> Thessalonians Two_2
+    14 -> Timothy Two_1
+    15 -> Timothy Two_2
+    16 -> Titus
+    17 -> Philemon
+    18 -> Hebrews
+    19 -> James
+    20 -> Peter Two_1
+    21 -> Peter Two_2
+    22 -> JohnEpistle Three_1
+    23 -> JohnEpistle Three_2
+    24 -> JohnEpistle Three_3
+    25 -> Jude
+    26 -> Revelation
+    n -> error $"NewTestament has no book at index" <> show n
 
 type NewTestament = NewTestament' Identity
+
+newTestamentToString
+  :: (Two -> String -> String)
+  -> (Three -> String -> String)
+  -> NewTestament' Identity
+  -> String
+newTestamentToString showTwo showThree x = case x of
+  Matthew -> "Matthew"
+  Mark -> "Mark"
+  Luke -> "Luke"
+  John -> "John"
+  Acts -> "Acts"
+  Romans -> "Romans"
+  Corinthians x -> showTwo x "Corinthians"
+  Galatians -> "Galatians"
+  Ephesians -> "Ephesians"
+  Philippians -> "Philippians"
+  Colossians -> "Colossians"
+  Thessalonians x -> showTwo x "Thessalonians"
+  Timothy x -> showTwo x "Timothy"
+  Titus -> "Titus"
+  Philemon -> "Philemon"
+  Hebrews -> "Hebrews"
+  James -> "James"
+  Peter x -> showTwo x "Peter"
+  JohnEpistle x -> showThree x "John"
+  Jude -> "Jude"
+  Revelation -> "Revelation"
 
 data Canon' f
   = Canon_Old (OldTestament' f)
@@ -159,11 +364,22 @@ deriving instance Show (Canon' Ignore)
 deriving instance Show (Canon' Identity)
 instance Universe (Canon' Ignore) where universe = universeGeneric
 instance Universe.Finite (Canon' Ignore)
-instance Universe (Canon' Identity) where universe = universeGeneric
+instance Universe (Canon' Identity) -- Uses 'Enum' instance
 instance Universe.Finite (Canon' Identity)
 instance Bounded (Canon' f) where
   minBound = Canon_Old minBound
   maxBound = Canon_New maxBound
+instance Enum (Canon' Identity) where
+  fromEnum x = case x of
+    Canon_Old b -> fromEnum b
+    Canon_New b -> 1 + fromEnum b + fromEnum (maxBound :: OldTestament' Identity)
+  toEnum x
+    | x <= lastOtIndex = Canon_Old $ toEnum x
+    | otherwise = Canon_New $ toEnum $ x - lastOtIndex - 1
+    where
+      lastOtIndex = fromEnum (maxBound :: OldTestament' Identity)
+
+type Canon = Canon' Identity
 
 data Apocrypha' f
   = Tobit
