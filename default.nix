3c5c680ba3afb583e14b0b5859e11d0b5d8595c0
Elliot Cameron <eacameron@gmail.com>
2020-03-14 00:52:18 -0400

    Accept Android SDK license

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
     # You must accept the Android Software Development Kit License Agreement at
     # https://developer.android.com/studio/terms in order to build Android apps.
     # Uncomment and set this to `true` to indicate your acceptance:
-    # config.android_sdk.accept_license = false;
+    config.android_sdk.accept_license = true;
   }
 }:
 with obelisk;
